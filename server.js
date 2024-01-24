31f07371bd85d0181b28e2017e26116f817e1f71
sousa-p <p.soousa7@gmail.com>
2024-01-24 14:05:45 -0300

    feat: Initial structure

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 'use strict'
+
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+const route = router.get('/', (req, res, next) => {
+  res.status(200).send({
+    title: "Node Store API",
+  });
+});
+
+app.use('/', route);
+app.listen(port);
