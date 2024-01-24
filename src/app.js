b7eed09e6e69fb98423ece6c2e7d21fb2ae6f3ee
sousa-p <p.soousa7@gmail.com>
2024-01-24 16:17:32 -0300

    feat: add body parser

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const route = router.get('/', (req, res, next) => {
   res.status(200).send({
