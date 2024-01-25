19d890e56c66573447bfdc42cf0fcac82cf0d63c
sousa-p <p.soousa7@gmail.com>
2024-01-25 10:57:58 -0300

    feat: Add mongoose and db connection

--- src/app.js
+++ src/app.js
@@ -2,15 +2,27 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
 
+mongoose.connect('mongodb://localhost:27017/nodeStore');
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+db.on('error', (err) => {
+  console.error('Error connection to MongoDB:', err);
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 
 app.use('/', indexRoute);
 app.use('/product', productRoute);
