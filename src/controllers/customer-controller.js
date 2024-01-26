5d61a04c9c1b63bb1f3e1594000840d81e1b7e1f
sousa-p <p.soousa7@gmail.com>
2024-01-26 16:08:23 -0300

    feat:  add costumer routes

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -3,10 +3,28 @@
 const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/customer-repository');
 
-exports.post = async (req, res, next) => {
-  const contract = new ValidationContract();
-  const data = req.body;
+exports.getAll = async (req, res, next) => {
+  try {
+    const customers = await repository.getAll();
+    return res.status(200).send(customers);
+  } catch (e) {
+    return res.status(400).send(e);
+  }
+}
 
+exports.getById = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const customer = await repository.getById(id);
+    return res.status(200).send(customer);
+  } catch (e) {
+    return res.status(400).send(e);
+  }
+}
+
+exports.post = async (req, res, next) => {
+  const data = req.body;
+  const contract = new ValidationContract();
   contract.hasMinLen(data.name, 3, 'The customer\'s name must contain at least 3 letters');
   contract.isEmail(data.email, 'The customer\'s E-mail must be valid');
   contract.hasMinLen(data.password, 8, 'The customer\'s password must contain at least 8 letters');
@@ -15,7 +33,7 @@ exports.post = async (req, res, next) => {
 
   try {
     const customer = await repository.post(data);
-    return res.status(201).send({ message: 'The customer has been created successfully!'});
+    return res.status(201).send({ message: 'The customer has been created successfully!' });
   } catch (e) {
     return res.status(400).send(e);
   }
--- src/repositories/customer-repository.js
+++ src/repositories/customer-repository.js
@@ -4,12 +4,22 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const Customer = mongoose.model('Customer');
 
+exports.getAll = () => {
+  let customers = Customer.find({}, 'name email');
+  return customers;
+}
+
+exports.getById = (id) => {
+  let customer = Customer.findById(id, 'name email');
+  return customer;
+}
+
 exports.post = async (data) => {
   let customer = new Customer();
-  
+
   customer.name = data.name;
   customer.email = data.email;
-  customer.password =  await bcrypt.hash(data.password, 12);
+  customer.password = await bcrypt.hash(data.password, 12);
 
   return customer.save();
 }
